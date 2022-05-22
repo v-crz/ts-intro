c4bc13b6889312da0779ae6d553f4a8045c28c9a
VC <vbatten10@hotmail.com>
2022-05-21 21:18:51 -0500

    Add: Tipos basicos y conceptos generales

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Viridiana';
+let hp: number | string = 95;
+let estaVivo: boolean = false;
+
+hp = 'FULL';
+
+console.log(nombre, hp);
+
